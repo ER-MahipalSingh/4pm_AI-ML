839905e4bf3aca1bfe8d7c7c1ffe264aed0a52c9
MAHIPAL SINGH <rwr1.mahipal.sb@gmail.com>
2026-08-25 17:06:29 +0530

    Add files via upload

---
+++ python/11.py
@@ -0,0 +1,73 @@
+# data = [
+#     [1,2,3],
+#     [4,5,6]
+# ]
+
+# print(type(data))
+# print(data[0][2])
+
+# for row in data:
+#     for col in row:
+#         print(col, end=" ")
+#     print()
+    
+
+# data = (
+#     {10,20,30},
+#     {40,50,60},
+#     [100,500],
+#     (1,2,3),
+#     {"tech":"python"}
+# )
+
+# print(type(data))
+# print(data[3][1])
+
+# for row in data:
+#     for col in row:
+#         print(col, end=" ")
+#     print()
+
+# data = [
+#     {"tech":"python", "version":3.15},
+#     {"tech":"java", "version":15},
+# ]
+
+# print(data)
+
+# for row in data:
+#     for key, value in row.items():
+#         print(key, value)
+#     print()
+
+# data = {
+#     1:{
+#         "name":"jhon", "age":20
+#     },
+#     2:{
+#         "name":"david", "age":22
+#     }
+# }
+# print(data)
+# print(type(data))
+
+# for id, row in data.items():
+#     print(id)
+#     for key, value in row.items():
+#         print(key, value)
+#     print()
+
+
+# a = (1,2)
+# b = list(a)
+# c = dict(a)
+# d = ["name", "python", "version", 20]
+# e = dict(d)
+
+# print(a)
+# print(b)
+# print(type(a))
+# print(type(b))
+
+# print(e)
+# print(type(e))
